e2177dbcb76239436f28e94c907602fc02325d32
Pako Arroyo <francisco.arroyo@controlzeta.com.mx>
2022-03-16 11:43:27 -0600

    NgModel Binding

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { Title } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
