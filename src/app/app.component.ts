a3ad051323e6221c669c3a452e447bd22f6115c4
Pako Arroyo <francisco.arroyo@controlzeta.com.mx>
2022-03-15 21:02:50 -0600

    First Class

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Title } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,15 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
+
+  constructor(private tileService : Title) {
+      tileService.setTitle(this.charge);
+  }
+
   title = 'AngularTest';
+  name : string = 'Francisco Arroyo';
+  charge: string = 'Fullstack .Net Developer';
+  imageSrc: string = 'https://controlzeta.github.io/img/bio.jpg';
+  btnDisabled = true;
+  progressBar = 99;
 }
