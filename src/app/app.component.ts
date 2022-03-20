f062ba733830d3892bbe18726028fcb9989e8412
Pako Arroyo <francisco.arroyo@controlzeta.com.mx>
2022-03-20 08:14:00 -0600

    ngSwitch and images

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -17,7 +17,7 @@ export class AppComponent {
   title = 'AngularTest';
   name : string = 'Francisco Arroyo';
   charge: string = 'Fullstack .Net Developer';
-  imageSrc: string = 'https://controlzeta.github.io/img/bio.jpg';
+  imageSrc: string = '../assets/img/bio.jpg';
   description: string = "I'm a Fullstack .Net Developer focused on C# for its hardiness, scalability and interoperability with other systems, I have also worked with PHP, Angular JS and VB making improvements, migrations and repairs of existing systems. I'm able to work in teams, remotely or on site.' I love to keep me updated on new hardware and software solutions, either through online courses, lectures, attend to webinars, read blogs or classroom courses is always better to know what new technologies you can take for your work.";
   newTech: string = 'AngularJs';
   year: number = new Date().getFullYear();
